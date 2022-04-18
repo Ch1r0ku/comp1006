399bd51c9fde4528fde9d2d3a93868fdab4e6e55
RomanLys <90863345+Ch1r0ku@users.noreply.github.com>
2022-04-18 09:41:29 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,28 @@ function comparePasswords() {
         return true;
     }
 }
+// looping gallery
+function galleryArray() {
+    var gallery = document.getElementById("output");
+    for (i = 0; i < imgArray.length; i++) {
+        gallery.appendChild(imgArray[i]);
+    }
+};
+var imgArray = new Array();
+imgArray[0] = new Image();
+imgArray[0].src = 'img/game-uploads/ptr2bnkodujtj5ht4uanobp5pm-activision-tennis.jpg';
+
+imgArray[1] = new Image();
+imgArray[1].src = 'img/game-uploads/ptr2bnkodujtj5ht4uanobp5pm-dark-souls.jpg';
+
+imgArray[2] = new Image();
+imgArray[2].src = 'img/game-uploads/ptr2bnkodujtj5ht4uanobp5pm-last-of-us.jpg';
+
+imgArray[3] = new Image();
+imgArray[3].src = 'img/game-uploads/ptr2bnkodujtj5ht4uanobp5pm-madden-21.jpg';
+
+imgArray[4] = new Image();
+imgArray[4].src = 'img/game-uploads/ptr2bnkodujtj5ht4uanobp5pm-nhl-2k11.jpg';
+
+imgArray[5] = new Image();
+imgArray[5].src = 'img/game-uploads/ptr2bnkodujtj5ht4uanobp5pm-wow.jpg';
