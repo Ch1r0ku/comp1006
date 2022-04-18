bf1d0d0f5496f236e457e38ca59e721a95f4e3a6
RomanLys <90863345+Ch1r0ku@users.noreply.github.com>
2022-04-18 10:43:35 -0400

    Update save-logo.php

--- save-logo.php
+++ save-logo.php
@@ -9,26 +9,31 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    echo "File is an image - " . $check["mime"] . ". ";
     $isgoodtoupload = 1;
   } else {
-    echo "File is not an image.";
+    echo "File is not an image. ";
     $isgoodtoupload = 0;
   }
 };
 
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "File too large. ";
+    $uploadOk = 0;
+  }
+
 if($imageFileType != "png") {
-  echo "PNG files only!";
+  echo "PNG files only! ";
   $isgoodtoupload = 0;
 };
 
 if ($isgoodtoupload == 0) {
-    echo "File not uploaded, try again!";
+    echo "File not uploaded, try again! ";
   } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-      echo "New logo ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+      echo "New logo ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded. ";
     } else {
-      echo "Sorry, there was an error uploading your file, try again!";
+      echo "Sorry, there was an error uploading your file, try again! ";
     }
   };
 ?>
